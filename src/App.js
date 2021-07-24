b5683eb36d77e2bc51b94d5d11a0b943c29c4684
Jen <52062192+jendevelops@users.noreply.github.com>
2021-07-23 23:47:05 -0700

    Add slides Add h1 slides and styling to create full screen slides with different styling for each slide.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 className="slide first">Hi! I'm slide 1!</h1>
+      <h1 className="slide second">Hi! I'm slide 2!</h1>
+      <h1 className="slide">Hi! I'm slide 3!</h1>
     </div>
   );
 }
